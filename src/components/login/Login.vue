a4def7d7274d05e046234beddada239ef8451638
vitebo <vitebo@hotmail.com>
2018-05-05 17:42:30 -0300

    creating login

--- src/components/login/Login.vue
+++ src/components/login/Login.vue
@@ -1,11 +1,29 @@
 <template>
-  <div>
+  <div @keyup.enter="efetuarLogin(user)">
       <h2>PAGINA DE LOGIN</h2>
+      <input v-model="user.login" type="text" placeholder="login">
+      <input v-model="user.password" type="password" placeholder="password">
+      <button @click="efetuarLogin(user)">Login</button>
   </div>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      user: {}
+    }
+  },
+  methods: {
+    efetuarLogin (user) {
+     this.$http
+      .get(`http://localhost:3000/users?login=${user.login}&password=${user.password}`)
+      .then(response => response.json())
+      .then(response => {
+        if (response.length) this.$router.push('home')
+      })
+    }
+  }
 }
 </script>
 
