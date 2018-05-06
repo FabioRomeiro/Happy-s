1dc3a98d21410a19dc473b747dec1135b794354c
FabioRomeiro <flromeiroc@gmail.com>
2018-05-05 22:47:54 -0300

    Adiconando load paralelos no feed

--- src/components/login/Login.vue
+++ src/components/login/Login.vue
@@ -1,18 +1,32 @@
 <template>
-  <div class="paginal-principal">
+  <div class="pagina-principal">
     <div @keyup.enter="efetuarLogin(user)" class="login-corpo">
+        <h2>Entre em sua conta:</h2>
         <input v-model="user.login" type="text" placeholder="login">
         <input v-model="user.password" type="password" placeholder="password">
         <button @click="efetuarLogin(user)">Login</button>
+    </div>
+    <div class="banner-corpo"> <!--FAZER COMPONENTE SEPARADO-->
+      <h1>[NomeDoProjeto]</h1>
+      <p>Momentos de tristeza e dificuldade são momentos de fragilidade.</p>
+      <p class="banner-fade-palavras">
+        <span>Uma</span> 
+        <span class="keyword"><vue-typer :text='[message[0], message[1], message[2]]' :shuffle='true'></vue-typer></span>
+      </p>
+      <p>é sempre bem vinda nessas situações...</p>
+      <p>Pensando na sua felicidade e tranquilidade, [a/o] [NomeDoProjeto] concentra estas pequenas palavras que te trarão um grande conforto</p>
+      <p></p>
     </div>
   </div>
 </template>
 
 <script>
+import { VueTyper } from 'vue-typer'
 export default {
   data () {
     return {
-      user: {}
+      user: {},
+      message : ['palavra amiga', 'experiência', 'história']
     }
   },
   methods: {
@@ -27,6 +41,9 @@ export default {
           }
         })
     }
+  },
+  components : {
+    VueTyper
   }
 }
 </script>
